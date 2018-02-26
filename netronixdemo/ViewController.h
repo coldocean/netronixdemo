cf32ce17d4260db07d55f60091ae07be5dd70f72
Oleg Sitovs <oleg.sitovs@netstart-systems.de>
2018-02-26 10:38:12 +0200

    Initial Commit

---
+++ netronixdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  netronixdemo
+//
+//  Created by Oleg Sitovs on 26/02/2018.
+//  Copyright © 2018 Oleg Sitovs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ netronixdemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  netronixdemo
+//
+//  Created by Oleg Sitovs on 26/02/2018.
+//  Copyright © 2018 Oleg Sitovs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
