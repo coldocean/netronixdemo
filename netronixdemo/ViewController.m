481e2b0843610eb6e1b5471adaefd0b49f2244ec
Oleg Sitovs <oleg.sitovs@netstart-systems.de>
2018-02-28 16:02:49 +0200

    add table cells

--- netronixdemo/ViewController.m
+++ netronixdemo/ViewController.m
@@ -12,12 +12,15 @@
 
 @property (nonatomic, retain) IBOutlet UITableView *tableView;
 
+@property (nonatomic, retain) NSMutableArray *dataSource;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _dataSource = [NSMutableArray array];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,6 +29,46 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _dataSource.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = nil;
+    NSString *CellIdentifier = @"UITableViewCell";
+    cell = (UITableViewCell *)[tView dequeueReusableCellWithIdentifier:CellIdentifier];
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc] init];
+            [cell awakeFromNib];
+        }
+    return cell;
+}
+
+-(CGFloat)tableView:(UITableView*)tableView heightForRowAtIndexPath:(NSIndexPath*)indexPath {
+    return 44.0;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 0.01;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return 0.0;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tView deselectRowAtIndexPath:indexPath animated:NO];
+}
+
 
 
 @end
